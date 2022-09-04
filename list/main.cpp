e3f76dea9a3eb3b4f9c4925e27d110215d35abc5
Juan Diego Castro Padilla <79115974+ByJuanDiego@users.noreply.github.com>
2022-09-04 15:04:49 -0500

    _

--- list/main.cpp
+++ list/main.cpp
@@ -6,12 +6,12 @@ using std::boolalpha;
 
 int main() {
     forward_list<int> fl ({1, 2, 3, 4}, false);
-    cout << "forward list: ";
+    cout << "forward list: " << endl;
     for (const int& i: fl){
         cout << i << ' ';
     }
     cout << endl;
-    cout << "bidirectional list: ";
+    cout << "bidirectional list: " << endl;
     bidirectional_list<int> bl {1, 2, 3, 4};
     for (const int& i : bl){
         cout << i << ' ';
